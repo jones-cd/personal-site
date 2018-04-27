84f02d24dbf4838891721a8121d6b743663d73b3
Chris <releasethebadgers@yahoo.co.uk>
2018-04-27 16:18:08 +0100

    jquery tweak, renamed .hml file

--- my_script.js
+++ my_script.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
 
-
     $(".portfolio-item").click(function(){
-      $(".para1").fadeToggle(700);
-      $(this).children("p-portfolio").addClass("p-portfolio-change")
-  		})
-
-     // $(".portfolio-item").mouseleave(function(){
-     //  $(".para1").fadeOut(700);
-  	// })
-
-      
-
-
+      $(this).children(".para1").slideToggle(1000);
+  	})
+  	
 });
