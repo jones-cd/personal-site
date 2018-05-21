a3b3d4eefedf5433cb2dd99e5d186ebcae690f3a
Chris <releasethebadgers@yahoo.co.uk>
2018-05-21 21:06:47 +0100

    Removed cv page, added lengthier biog section

--- my_script.js
+++ my_script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
+    $("#more-heading").click(function(){
+		$(".skills").slideToggle(1000);
+  	})
+
     $(".portfolio-item").click(function(){
       $(this).children(".para1").slideToggle(1000);
   	})
-  	
+
 });
